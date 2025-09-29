229c48cf109ba5e5c9538c472419f23124edc80e
pcaliman2 <pcaliman@gmail.com>
2025-09-29 01:25:00 -0400

    Conexion_w_env_02

--- Analisis.bat
+++ Analisis.bat
@@ -1,4 +1,12 @@
 @echo off
 cd /d "%~dp0"
-start "" /b "inventario_4.exe"
+
+REM variables de entorno
+set INVENTARIO_DB_USER=postgres
+set INVENTARIO_DB_PASSWORD=...
+set INVENTARIO_DB_HOST=nozomi.proxy.rlwy.net
+set INVENTARIO_DB_PORT=59582
+set INVENTARIO_DB_NAME=railway
+
+start "" /b "%~dp0inventario_4.exe"
 exit
